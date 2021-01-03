bb66a9ab1b70d67b24263f27b9a00d9c54d2a415
Artiom <64665123+qbeeck@users.noreply.github.com>
2021-01-03 18:55:56 +0200

    add, close

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, {useState} from 'react';
-import Header from '../src/components/Header/index.jsx';
-import AddList from '../src/components/City-block/index.jsx';
+import React, { useState } from "react";
+import Header from "../src/components/Header/index.jsx";
+import AddList from "../src/components/City-block/index.jsx";
 
 function App() {
   const [lists, setLists] = useState([]);
 
-  const onAddList = (obj) => { 
+  const onAddList = (obj) => {
     const newList = [...lists, obj];
     setLists(newList);
+  };
+
+  const refreshList = () => {
+    const newList = [...lists];
+    setLists(newList);
+  }
+
+  const onRemoveList = (obj) => {
+    let pos = lists.findIndex(item => item.id == obj.id);
+    let newList = lists;
+    newList.splice(pos, 1);
+    refreshList();
   };
 
   return (
     <div className="weather-app">
       <div className="weather-app__header">
-        <Header onAdd={onAddList}/>
+        <Header onAdd={onAddList} />
       </div>
-      <div className="weather-app__alert"></div>
       <div className="weather-app__city-block">
-        <AddList cities={lists} />
+        <AddList cities={lists} removeCity={(item) => onRemoveList(item)} />
       </div>
     </div>
   );
--- src/components/City-block/index.jsx
+++ src/components/City-block/index.jsx
@@ -3,14 +3,14 @@ import React from "react";
 import "./City-block.scss";
 import removeButtonSvg from "../assets/remove.svg";
 
-const AddCity = ({ cities }) => {
+const AddCity = ({ cities, removeCity }) => {
   return (
     <ul className="city-list">
       {cities.map((item) => (
         <li key={item.id} className="city-list__city">
           <span className="city__span-city">{item.name}</span>
           <span className="city__span-temperature">{item.temperature}°С</span>
-          <img className="city__img-remove-button" src={removeButtonSvg} alt="del"></img>
+          <img onClick={() => removeCity(item)} className="city__img-remove-button" src={removeButtonSvg} alt="del"></img>
         </li>
       ))}
     </ul> 
