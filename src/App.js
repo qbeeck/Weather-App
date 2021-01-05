5ba824c6c78635a16a65e983dc6ad0d9abb8e748
Artiom <64665123+qbeeck@users.noreply.github.com>
2021-01-05 16:13:14 +0200

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import { HashRouter, Route, Link } from "react-router-dom";
 import Header from "../src/components/Header/index.jsx";
-import AddList from "../src/components/City-block/index.jsx";
 import Modal from "../src/components/Modal/index.jsx";
+import AddList from "../src/components/City-block/index.jsx";
 
 import closeButtonSvg from "../src/components/assets/close.svg";
 
@@ -33,30 +34,36 @@ function App() {
   };
 
   return (
-    <div className="weather-app">
-      <div className="weather-app__header">
-        <Header onAdd={onAddList} />
-      </div>
-      {visibleModal && (
-        <div className="weather-app__modal">
-          <img
-            onClick={() => setVisibleModal(false)}
-            src={closeButtonSvg}
-            className="modal__return-button"
-            alt="close"
-          />
-          <Modal modalGive={resultObj} />
+    <HashRouter basename="/">
+      <div className="weather-app">
+        <div className="weather-app__header">
+          <Header onAdd={onAddList} />
         </div>
-      )}
-      <div className="weather-app__city-block">
-        <AddList
-          result={(obj) => openModal(obj)}
-          cities={lists}
-          removeCity={(item) => onRemoveList(item)}
-        />
+        {visibleModal && (
+          <div className="weather-app__modal">
+            <img
+              onClick={() => setVisibleModal(false)}
+              src={closeButtonSvg}
+              className="modal__return-button"
+              alt="close"
+            />
+            <Modal modalGive={resultObj} />
+          </div>
+        )}
+        <div className="weather-app__city-block">
+          <AddList
+            result={(obj) => openModal(obj)}
+            cities={lists}
+            removeCity={(item) => onRemoveList(item)}
+          />
+        </div>
       </div>
-    </div>
+    </HashRouter>
   );
 }
+
+const Header = () => <div><h2>Header</h2></div>
+const Modal = () => <div><h2>Modal</h2></div>
+const AddList = () => <div><h2>AddList</h2></div>
 
 export default App;
